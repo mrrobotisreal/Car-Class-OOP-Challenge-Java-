6aa7e2150c4107179ab882692046551776519630
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-16 15:10:13 -0600

    Wrote a very simple vehicle class with fields and methods.

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,22 @@
+import java.sql.SQLOutput;
+
 public class Vehicle {
+    private boolean hasGasEngine = true;
+    private int numOfDoors;
+    private String color;
+    private int maxSpeed;
+
+    public void Vehicle(int doors, String color, int maxSpeed) {
+        this.numOfDoors = doors;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public void Drive(int speed) {
+        System.out.println("Vehicle is driving at " + speed + "mph.");
+    }
+
+    public void Turn(String direction) {
+        System.out.println("Vehicle is turning " + direction + ".");
+    }
 }
